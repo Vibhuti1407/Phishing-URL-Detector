7dee30e0ac56499aad45fa971f9f4b382986523b
VIbhuti Naik <vibhutinaik1407@gmail.com>
2026-01-28 22:16:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-TRUSTED_DOMAINS = ['google.com', 'microsoft.com', 'github.com', 'apple.com', 'wikipedia.org','login']
+TRUSTED_DOMAINS = ['google.com', 'microsoft.com', 'github.com', 'apple.com', 'wikipedia.org','dribbble.com/login.php']
 
 def is_whitelisted(url_input):
     domain = urlparse(url_input).netloc.lower()
@@ -129,3 +129,4 @@ with right_col:
         else:
 
             st.warning("Please enter a URL.")
+
